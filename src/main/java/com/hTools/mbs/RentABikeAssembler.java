adb8662f96fd468d1bdb4e93aaa700edd41e690d
Hyrum Steffensen <hyrum.steffensen@gmail.com>
2017-08-11 20:02:13 -0700

    wired dependencies with spring

--- src/main/java/com/hTools/mbs/RentABikeAssembler.java
+++ src/main/java/com/hTools/mbs/RentABikeAssembler.java
@@ -1,10 +1,11 @@
 package com.hTools.mbs;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class RentABikeAssembler {
 	public static final void main(String[] args) {
-		CommandLineView clv = new CommandLineView();
-		RentABikeService rentaBike = new RentABikeImpl("Bruce's Bikes");
-		clv.setRentaBike(rentaBike);
+		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("application-context.xml");
+		CommandLineView clv = (CommandLineView)ctx.getBean("commandLineView");
 		clv.printAllBikes();
 	}
 
--- src/main/java/com/hTools/mbs/RentABikeImpl.java
+++ src/main/java/com/hTools/mbs/RentABikeImpl.java
@@ -9,10 +9,27 @@ public class RentABikeImpl implements RentABikeService {
 	private String storeName;
 	final List<Bike>bikes = new ArrayList<Bike>();
 	
-	public RentABikeImpl(String storeName) {
+	public String getStoreName() {
+		return storeName;
+	}
+
+	public void setStoreName(String storeName) {
+		this.storeName = storeName;
+	}
+	
+	private void initBikes() {
 		bikes.add(new Bike("Shimano", "Roadmaster", 20, "11111", 15, "Fair"));
 		bikes.add(new Bike("Cannondale", "F2000 XTR", 18, "22222", 12, "Excellent"));
 		bikes.add(new Bike("Trek", "6000", 19, "33333", 12.4, "Fair"));
+	}
+
+	public RentABikeImpl() {
+		initBikes();
+	}
+	
+	public RentABikeImpl(String storeName) {
+		this.storeName = storeName;
+		initBikes();
 	}
 
 	@Override
