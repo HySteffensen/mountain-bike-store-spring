5ee3f4969d72fb4475bfcbaebdd2b26159453b1d
Hyrum Steffensen <hyrum.steffensen@gmail.com>
2017-08-11 20:18:58 -0700

    closed application context in RentABikeAssembler

--- src/main/java/com/hTools/mbs/RentABikeAssembler.java
+++ src/main/java/com/hTools/mbs/RentABikeAssembler.java
@@ -1,5 +1,6 @@
 package com.hTools.mbs;
 
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class RentABikeAssembler {
@@ -7,6 +8,7 @@ public class RentABikeAssembler {
 		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("application-context.xml");
 		CommandLineView clv = (CommandLineView)ctx.getBean("commandLineView");
 		clv.printAllBikes();
+		((ConfigurableApplicationContext)ctx).close();
 	}
 
 }
